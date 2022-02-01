5ea45cf21c1ac723a9ce31eb4e881f6d317498c2
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-01 14:43:51 -0500

    set up structure, nav and footer files

---
+++ components/Layout.jsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = ({children}) => {
+  return <div>
+    <Navbar />
+    {children}
+    <Footer />
+  </div>;
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+  return <div>
+    navbar
+  </div>;
+};
+
+export default Navbar;
