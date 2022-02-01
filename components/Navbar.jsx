b7a5de715d589ccb7432ca7fd3ea721be8d975b0
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-01 14:59:31 -0500

    draft style for nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
-  return <div>
-    navbar
+  return <div className={styles.container}>
+    <Link href="/">JH</Link>
+    <ul className={styles.list}>
+      <li className={styles.listItem}>
+        <Link href="/about">About</Link>
+      </li>
+      <li className={styles.listItem}>
+        <Link href="/skills">Skills</Link>
+      </li>
+      <li className={styles.listItem}>
+        <Link href="/projects">Projects</Link>
+      </li>
+      
+
+    </ul>
   </div>;
 };
 
