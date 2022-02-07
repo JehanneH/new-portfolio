f0dae4f5f379fab1ec5742bdb237b376211996a8
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-07 14:54:01 -0500

    set up styled components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,46 @@
 import Link from "next/link";
-import styles from "../styles/Navbar.module.css"
+// import styles from "../styles/Navbar.module.css"
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 50px;
+  background: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 100px;
+  font-size: 18px;
+`;
+
+const List = styled.ul`
+
+`
+const ListItem = styled.li`
+  display: inline;
+  margin-right: 30px;
+  font-size: 14px;
+  color: lightgrey;
+  cursor: pointer;
+`
 
 const Navbar = () => {
-  return <div className={styles.container}>
+  return <Container>
     <Link href="/">JH</Link>
-    <ul className={styles.list}>
-      <li className={styles.listItem}>
+    <List>
+      <ListItem>
         <Link href="/about">About</Link>
-      </li>
-      <li className={styles.listItem}>
+      </ListItem>
+      <ListItem>
         <Link href="/skills">Skills</Link>
-      </li>
-      <li className={styles.listItem}>
+      </ListItem>
+      <ListItem>
         <Link href="/projects">Projects</Link>
-      </li>
+      </ListItem>
       
 
-    </ul>
-  </div>;
+    </List>
+  </Container>;
 };
 
 export default Navbar;
