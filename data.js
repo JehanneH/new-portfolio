dcaf41bc09d95749d6a699ede9f56fb71b03bf8d
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-11 14:41:33 -0500

    add placeholder imgs and links to data

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 export const products = [
   {
     id: 1,
-    img: "",
+    img: "/img/wine.png",
     link: "https://github.com/JehanneH",
   },
   {
     id: 2,
-    img: "",
+    img: "/img/shopify.png",
     link: "https://github.com/JehanneH",
   },
   {
     id: 3,
-    img: "",
+    img: "/img/wine.png",
     link: "https://github.com/JehanneH",
   },
   {
     id: 4,
-    img: "",
+    img: "/img/shopify.png",
     link: "https://github.com/JehanneH",
   },
   {
     id: 5,
-    img: "",
+    img: "/img/wine.png",
     link: "https://github.com/JehanneH",
   },
   {
     id: 6,
-    img: "",
+    img: "/img/shopify.png",
     link: "https://github.com/JehanneH",
   },
 ];
