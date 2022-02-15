257d7edb879a52adf2f46d9429634e3041f1f721
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-15 15:46:01 -0500

    add content to contact and create contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,46 +1,66 @@
-import styled from "styled-components"
-import {MdLocationOn, MdAlternateEmail} from "react-icons/md"
-
-
+import styled from "styled-components";
+import { MdLocationOn, MdAlternateEmail } from "react-icons/md";
 
 const ContactContainer = styled.div`
   height: 100vh;
   position: relative;
-`
+`;
 const CBackground = styled.div`
   width: 20px;
   height: 100%;
   background-color: lavender;
   position: absolute;
-`
+`;
 const CWrapper = styled.div`
   padding: 50px;
   display: flex;
-`
+`;
 
 const CLeft = styled.div`
   flex: 1;
-`
+`;
 
 const CTitle = styled.h1`
   font-size: 60px;
   width: 80%;
-`
-const CInfo = styled.div`
-   
-`
+`;
+const CInfo = styled.div``;
+
 const CInfoItem = styled.div`
-   
-`
+  display: flex;
+  align-items: center;
+  margin: 50px 0px;
+
+`;
 
 const CRight = styled.div`
-  flex: 1;  
-`
+  flex: 1;
+`;
 const EmailIcon = styled(MdAlternateEmail)`
+  width: 30px;
+  height: 30px;
+  margin-right: 15px;
+`;
+const LocationIcon = styled(MdLocationOn)`
+  width: 30px;
+  height: 30px;
+  margin-right: 15px;
+`;
+
+const RDesc = styled.p`
+  font-weight: 300;
+`
+
+const Form = styled.form`
   
 `
-const LocationIcon = styled(MdLocationOn)``
 
+const Input = styled.input`
+  
+`
+const TextArea = styled.textarea`
+  
+`
 
 const Contact = () => {
   return (
@@ -48,18 +68,32 @@ const Contact = () => {
       <CBackground></CBackground>
       <CWrapper>
         <CLeft>
-          <CTitle>Feel free to contact me</CTitle>
+          <CTitle>Please feel free to reach out!</CTitle>
           <CInfo>
             <CInfoItem>
-              <EmailIcon /> samarahjhodge@gmail.com
-              <LocationIcon/> Montreal, Canada
+              <EmailIcon />
+              samarahjhodge@gmail.com
+            </CInfoItem>
+            <CInfoItem>
+              <LocationIcon />
+              Montreal, Canada
             </CInfoItem>
           </CInfo>
         </CLeft>
-        <CRight></CRight>
+        <CRight>
+          <RDesc>
+            Get in touch if you are interested in working with me
+          </RDesc>
+          <Form>
+            <Input type='text' placeholder="Name" name="user_name"></Input>
+            <Input type='text' placeholder="Subject" name="user_subject"></Input>
+            <Input type='text' placeholder="Email" name="user_email"></Input>
+            <TextArea></TextArea>
+          </Form>
+        </CRight>
       </CWrapper>
     </ContactContainer>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
