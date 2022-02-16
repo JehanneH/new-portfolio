3e566f1620f20304c4f4f4a9eaf105f53226e492
Jehanne Hodge <samarahjhodge@gmail.com>
2022-02-16 18:32:45 -0500

    style footer and add links for socials

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,35 +1,94 @@
 import styled from "styled-components";
-import {SiLinkedin, SiGithub, SiAngellist} from 'react-icons/si'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { SiLinkedin, SiGithub, SiAngellist } from "react-icons/si";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Link from "next/link";
 
 const FContainer = styled.div`
   background-color: lavender;
   height: 30vh;
-`
-const SocialIcons = styled.div` 
-`
-const Brand = styled.div` 
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const SocialIcons = styled.div`
+  width: 15%;
+  display: flex;
+  justify-content: space-evenly;
+`;
 
-const LinkedIn = styled(SiLinkedin)``
+const Brand = styled.div`
+  margin-top: 1rem;
+`;
 
-const Github = styled(SiGithub)``
+const LinkedIn = styled(SiLinkedin)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  transition: transform .3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
-const AngelList = styled(SiAngellist)``
+const Github = styled(SiGithub)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  transition: transform .3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
-const Resume = styled(BsFillPersonLinesFill)``
+const AngelList = styled(SiAngellist)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  transition: transform .3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
+const Resume = styled(BsFillPersonLinesFill)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  transition: transform .3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
 const Footer = () => {
-  return <FContainer>
-    <SocialIcons>
-      <LinkedIn />
-      <Github />
-      <AngelList />
-      <Resume />
-    </SocialIcons>
-    <Brand>Created by Jehanne Hodge</Brand>
-  </FContainer>;
+  return (
+    <FContainer>
+      <SocialIcons>
+        <Link href="https://www.linkedin.com/in/jehanne-hodge-8657251b3/">
+          <a target="_blank">
+            <LinkedIn />
+          </a>
+        </Link>
+        <Link href="https://angel.co/u/jehanne-hodge">
+          <a target="_blank">
+            <AngelList />
+          </a>
+        </Link>
+        <Link href="https://github.com/JehanneH">
+          <a target="_blank">
+            <Github />
+          </a>
+        </Link>
+        <Link href="https://www.canva.com/design/DAECeK-dAJ0/QDBrJSeb_zlnDxHSnK_b6g/edit?category=tACZCki4tbY">
+          <a target="_blank">
+            <Resume />
+          </a>
+        </Link>
+      </SocialIcons>
+      <Brand>Created by Jehanne Hodge</Brand>
+    </FContainer>
+  );
 };
 
 export default Footer;
