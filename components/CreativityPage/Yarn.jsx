dd2332c18a8f0c79a5510ed3582c79399f36ee26
Jehanne Hodge <samarahjhodge@gmail.com>
2022-03-08 18:15:15 -0500

    set up object for knitting pictures

--- components/CreativityPage/Yarn.jsx
+++ components/CreativityPage/Yarn.jsx
@@ -4,21 +4,50 @@ const YarnContainer = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
-  /* align-items: center; */
-  justify-content: center ;
-  border: 1px solid red;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column ;
+  /* border: 1px solid red; */
 `;
 
-const YarnTitle  = styled.h3`
-font-weight: 400;
-
+const YarnTitle  = styled.h2`
+font-weight: 600;
+padding: 1rem;
 `
 
+const KnitDesc  = styled.p`
+font-weight: 400;
+padding: 0 6rem;
+`
+const CrochetDesc  = styled.p`
+font-weight: 00;
+padding: 0 6rem;
+`
+
+const KnitProjectsContainer = styled.div``
+
+
+const CrochetProjectsContainer = styled.div``
+
 const Yarn = () => {
-  return <YarnContainer>
+  return <YarnContainer id="yarn">
     <YarnTitle>
-      These are some of my knitting and crochet projects
+      Knitting and Crochet Projects
     </YarnTitle>
+    <KnitDesc>
+    I learned how to knit from my mom when I was about 10 years old.
+    <br />
+    Throughout my life, every now and then, I would pick it back up again for a bit. 
+    <br />
+    At the end of 2019 I really got back into it and haven&apos;t stopped since. 
+    </KnitDesc>
+    <KnitProjectsContainer></KnitProjectsContainer>
+    <CrochetDesc>
+    I taught myself how to crochet in 2021. I mainly enjoy making different creations with granny squares.
+    </CrochetDesc>
+    <CrochetProjectsContainer>
+
+    </CrochetProjectsContainer>
   </YarnContainer>;
 };
 
---
+++ knitting.js
@@ -0,0 +1,58 @@
+export const knitting = [
+  {
+    id: 1,
+    img: "/creativity/knitting/knitting.jpeg",
+  },
+  {
+    id: 2,
+    img: "/creativity/knitting/blanket1.jpeg",
+  },
+  {
+    id: 3,
+    img: "/creativity/knitting/blanket2.jpeg",
+  },
+  {
+    id: 4,
+    img: "/creativity/knitting/blanket3.jpeg",
+  },
+  {
+    id: 5,
+    img: "/creativity/knitting/blanket4.jpeg",
+  },
+  {
+    id: 6,
+    img: "/creativity/knitting/neckwarmer1.jpeg",
+  },
+  {
+    id: 7,
+    img: "/creativity/knitting/neckwarmer2.jpeg",
+  },
+  {
+    id: 8,
+    img: "/creativity/knitting/balaclava1.jpeg",
+  },
+  {
+    id: 9,
+    img: "/creativity/knitting/balaclava2.jpeg",
+  },
+  {
+    id: 10,
+    img: "/creativity/knitting/balaclava3.jpeg",
+  },
+  {
+    id: 11,
+    img: "/creativity/knitting/balaclava4.jpeg",
+  },
+  {
+    id: 12,
+    img: "/creativity/knitting/balaclava5.jpeg",
+  },
+  {
+    id: 13,
+    img: "/creativity/knitting/balaclava6.jpeg",
+  },
+  {
+    id: 14,
+    img: "/creativity/knitting/balaclava7.jpeg",
+  },
+];
